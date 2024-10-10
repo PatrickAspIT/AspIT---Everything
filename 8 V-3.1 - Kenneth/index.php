f73d8c4d2a79fe39628e53028fcc5f3ad74eba39
Patrick <150119622+PatrickAspIT@users.noreply.github.com>
2024-10-10 09:00:23 +0200

    Add files via upload

---
+++ 8 V-3.1 - Kenneth/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP</title>
+</head>
+<body>
+  
+  <p> Link to Assigment 3: <a href="./Uge 2/opgave-3.php" target="_blank">Assignment 3</a> </p>
+  <p> Link to Assigment 4: <a href="./Uge 2/website-uden-php/" target="_blank">Assignment 4</a> </p>
+  <p> Link to Assigment 5.1-5: <a href="./Uge 2/opgave-5.php" target="_blank">Assignment 5.1-5</a> & Link to Assigment 5.6: <a href="./Uge 2/website-uden-php/" target="_blank">Assignment 5.6</a></p>
+  <p> Link to Assigment 6: <a href="./Uge 2/website-uden-php/" target="_blank">Assignment 6</a> </p>
+  <p> Link to Assigment 7: <a href="./Uge 2/website-uden-php/" target="_blank">Assignment 7</a> </p>
+  <p> Link to Assigment 8: <a href="./Uge 2/website-uden-php/" target="_blank">Assignment 8</a> </p>
+  <p> Link to Assigment 9: <a href="./Uge 2/website-uden-php/" target="_blank">Assignment 9</a> </p>
+  
+</body>
+</html>
